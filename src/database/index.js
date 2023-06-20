da7c94ea3799f5d6bfb795d505ae33ca1a18dcd5
Amauri Lima <amauri.plimaj@gmail.com>
2023-06-20 13:15:43 -0300

    feat: adds enviroment production

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,26 @@
 const { Client } = require('pg');
+require('dotenv').config();
 
-const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'mycontacts',
-});
+let client;
 
+if (process.env.NODE_ENV === 'production') {
+  // Modo de produção
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // Modo de desenvolvimento local
+  client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'root',
+    password: 'root',
+    database: 'mycontacts',
+  });
+}
 client.connect();
 
 exports.query = async (query, values) => {
