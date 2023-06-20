bfa48df262b8e3ce11e08d585bfb8954e659d55a
Amauri Lima <amauri.plimaj@gmail.com>
2023-06-20 14:22:30 -0300

    allow all cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -7,12 +7,12 @@ module.exports = function cors(request, response, next) {
   const origin = request.get('origin');
   const isAllowed = allowedOrigins.includes(origin);
 
-  if (isAllowed) {
-    response.setHeader('Access-Control-Allow-Origin', origin);
-    response.setHeader('Access-Control-Allow-Methods', '*');
-    response.setHeader('Access-Control-Allow-Headers', '*');
-    response.setHeader('Header-Control-Max-Age', '-1');
-  }
+  // if (isAllowed) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Header-Control-Max-Age', '-1');
+  // }
 
   next();
 };
